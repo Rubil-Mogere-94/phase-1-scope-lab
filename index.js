4db944a3971cfec76c315f124e96f69cb5f9da62
Rubil <mogererubil@gmail.com>
2025-03-14 14:40:53 +0300

    new commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+// 1. Global variable customerName
+var customerName = 'bob';
+
+// 2. Function to uppercase customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// 3. Function to set bestCustomer
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // No 'var', 'let', or 'const' makes it global
+}
+
+// 4. Function to overwrite bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// 5. Constant for least favorite customer
+const leastFavoriteCustomer = 'someone';
+
+// This function tries to change a const variable, which is not allowed
+function changeLeastFavoriteCustomer() {
+  // This will throw an error
+  leastFavoriteCustomer = 'new person';
+}
